61ad5415716c107a0489592514198d121baaa90e
Nevermind <137763987+lethanhphat1005@users.noreply.github.com>
2024-06-23 09:45:47 +0700

    addctrl

--- app/src/main/java/com/example/testgithub/MainActivity.java
+++ app/src/main/java/com/example/testgithub/MainActivity.java
@@ -14,12 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addControl();
     }
 
-    void addControl()
-    {
-        button = (Button) findViewById(R.id.button);
-    }
 
 }
