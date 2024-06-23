85fcabfa7a0a1ff995097ec6dd51d1639ef84853
Nevermind <137763987+lethanhphat1005@users.noreply.github.com>
2024-06-23 09:39:30 +0700

    addctrl

--- app/src/main/java/com/example/testgithub/MainActivity.java
+++ app/src/main/java/com/example/testgithub/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.testgithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addControl();
     }
+
+    void addControl()
+    {
+        button = (Button) findViewById(R.id.button);
+    }
+
 }
